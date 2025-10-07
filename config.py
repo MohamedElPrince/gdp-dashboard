e74b740897d22e8ea817be0b65fedb42e068b8e8
Mohamed ElPrince <mohamed.elprince@incorta.com>
2025-10-07 09:05:45 +0000

    Add other py files

---
+++ config.py
@@ -0,0 +1,6 @@
+INCORTA_CONFIG = {
+    'host': 'https://fusion.cloudstaging.incortalabs.com/incorta',
+    'schema': 'D86_POC',
+    'api_key': '+ui37sqitt4v4xUlr1pl9Sp8QZC0RM2e0dTeflgLQhIdfbeOwL7iibWoO9sAGF4IGnYSPKBTOvKB+Fxe5/cenxWhRl+GNw5D7dzXI/O7NTXg6vFTh2ovBp4wMehQIMHSEjn+6wk/+lYf/9aRGrtc2Q==',
+    'timeout': 30
+}
